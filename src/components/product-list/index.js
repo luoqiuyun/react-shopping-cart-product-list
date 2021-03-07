4da2789f198448f7a1b3c542226af2b52e2f8dab
luoqiuyun <qiuyun.luo@outlook.com>
2021-03-07 12:19:15 -0500

    update

---
+++ src/components/product-list/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import Product from './Product';
+
+export default class ProductList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            addedToCart: false
+        }
+    }
+
+    hideButton = () => {
+        this.setState({addedToCart: true});
+    }
+
+    render() {
+
+        return (
+            <div className="layout-row wrap justify-content-center flex-70 app-product-list">
+                {this.props.products.map((product, i) => 
+                    <Product 
+                        idx={i}
+                        key={'p-' + i}
+                        product={product}
+                        addToCart={this.props.addToCart}
+                        removeFromCart={this.props.removeFromCart} />
+                )}
+            </div>
+
+        );
+    }
+}
+
